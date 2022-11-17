4deb99802b458a36bce676d68a9345e6ac0922cc
rlawnaks <rlawnaks1234@naver.com>
2022-11-17 14:10:38 +0900

    게시판 등록 개발

---
+++ board/board_view.php
@@ -0,0 +1,7 @@
+<form action="service/createBoard.php" method="post">
+    <input type="text" name="title" placeholder="제목">
+    <br>
+    <textarea name="content" id="" cols="30" rows="10"></textarea>
+    <br>
+    <input type="submit" value="test">
+</form>
