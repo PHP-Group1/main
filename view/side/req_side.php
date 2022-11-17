d7de34772c6c647194a74784b1dd39b493b70d6a
최혜림 <hyerim@choehyelim.local>
2022-11-17 11:58:47 +0900

    side + original_navbar 추가
    side 변경부터 originl navbar까지 추가했습니다

---
+++ view/side/req_side.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Side</title>
+    <style>
+        body{
+            margin:0;
+            padding: 0;
+        }
+    
+        .Qna_content{
+            margin: 0;
+            padding: 0;
+            
+        }
+        .side{
+            position: absolute;
+            width: 294px;
+            height: 971px;
+            margin:0;
+            padding: 0;
+
+            background: #CFCCC9;
+        }
+        .QnA{
+            position: relative;
+            top: 140px;
+            left: 5%;
+
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 22px;
+        }
+        .req{
+            position: relative;
+            top: 170px;
+            left: 5%;
+
+            margin: 0;
+            padding: 0;
+            font-weight: 700; 
+            font-size: 18px;
+            line-height: 22px;
+        }
+        .employ{
+            position: relative;
+            top: 200px;
+            left: 5%;
+
+            margin: 0;
+            padding: 0;
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 22px;
+        }
+        .board{
+            position: relative;
+            top: 230px;
+            left: 5%;
+            
+            margin: 0;
+            padding: 0;
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 22px;
+        }
+        
+    </style>
+</head>
+<body>
+    <div class = "Qna_content">
+        <div class = "side">
+            <h3 class = "QnA">QnA</h3>
+            <p class = "req">요구사항 및 개선사항</p>
+            <p class = "employ">채용문의</p>
+            <p class = "board">게시판 문의</p>
+        </div>
+    </div>
+    
+</body>
+</html>
