52c3484ce2add1513609b0ffa29dd3c1b3e8ec74
최혜림 <hyerim@choehyelim.local>
2022-11-18 21:40:11 +0900

    navbar + request_contents
    페이지 이동 시 사용하는 navbar와 요구사항 및 개선사항 전송 페이지

---
+++ view/nav/nav_animal.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>animal navbar</title>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+        }
+        .navbar{
+            margin: 0;
+            padding: 0;
+            position: absolute;
+
+            width: 100%;
+            height: 70px;
+
+            background-color: #FFFDF5;
+        }
+        .todayLuck{
+            margin: 0;
+            padding: 0;
+            position: absolute;
+            top : 16px;
+            left: 10%;
+
+            font-weight: 400;
+            font-size: 28px;
+            line-height: 34px;
+            text-align: center;
+        }
+        .animal{
+            margin: 0;
+            padding: 0;
+            position: absolute;
+            top : 16px;
+            left: 25%;
+
+            font-weight: 400;
+            font-size: 28px;
+            line-height: 34px;
+            text-align: center;
+
+            border-bottom: 2px solid #000000;
+
+        }
+        .star{
+            margin: 0;
+            padding: 0;
+            position: absolute;
+            top : 16px;
+            left: 36%;
+
+            font-weight: 400;
+            font-size: 28px;
+            line-height: 34px;
+            text-align: center;
+
+        }
+        .listen{
+            margin: 0;
+            padding: 0;
+            position: absolute;
+            top : 16px;
+            left: 50%;
+
+            font-weight: 400;
+            font-size: 28px;
+            line-height: 34px;
+            text-align: center;
+
+        }
+        .fortune{
+            margin: 0;
+            padding: 0;
+            position: absolute;
+            top : 16px;
+            left: 66%;
+
+            font-weight: 400;
+            font-size: 28px;
+            line-height: 34px;
+            text-align: center;
+
+        }
+        .item{
+            margin: 0;
+            padding: 0;
+            position: absolute;
+            top : 16px;
+            left: 80%;
+
+            font-weight: 400;
+            font-size: 28px;
+            line-height: 34px;
+            text-align: center;
+
+        }
+    </style>
+</head>
+<body>
+    <div class = "navbar">
+        <div class = "todayLuck">오늘의 운세</div>
+        <div class = "animal">띠 운세</div>
+        <div class = "star">별자리 운세</div>
+        <div class = "listen" >듣고싶은 말</div>
+        <div class = "fortune">포춘쿠키</div>
+        <div class = "item">행운의 아이템</div>
+    </div>
+</body>
+</html>
