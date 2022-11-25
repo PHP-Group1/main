5001df288b7f6431bdd2133596f467251fb8d976
rlawnaks <rlawnaks1234@naver.com>
2022-11-25 18:41:32 +0900

    function

--- common/function.php
+++ common/function.php
@@ -1,7 +1,7 @@
 <?php
 
     require 'config/conn.php';
-    require '../../auth/service/cookie.php';
+    require '../auth/service/cookie.php';
 
     function sqlResult($sql) {
         global $conn;
@@ -19,4 +19,36 @@
         setcookie($cookieName, $value, time() + $available);
     }
 
+    function loginCheck($logined, $not) {
+        session_start();
+
+        if (!isset($_SESSION)) {
+            include $not;
+        } else {
+    
+            if ($_SESSION['user_id']) {
+                include $logined;
+            } else {
+                include $not;
+            }
+    
+        }
+    }
+
+    function header_check() {
+        
+        session_start();
+
+        if (!$_SESSION) {
+            include "header.php";
+        } else {
+    
+            if ($_SESSION['user_id']) {
+                include "login_header.php";
+            } else {
+                include "header.php";
+            }
+            
+        }
+    }
 ?>
